5bb8f3fd7688e7e3e90b3abd9d52bb612695e15c
Hatun05 <445201400@student.ksu.edu.sa>
2026-01-20 20:45:21 +0300

    hatun

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+function addIngredient() {
+  const div = document.createElement("div");
+  div.className = "row";
+  div.innerHTML = `
+    <input type="text" placeholder="Enter ingredient">
+    <input type="text" placeholder="Enter quantity">
+  `;
+  document.getElementById("ingredients").appendChild(div);
+}
+
+let stepCounter = 1;
+
+function addStep() {
+  stepCounter++;
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "Step " + stepCounter + ": Enter instructions";
+  input.style.marginBottom = "10px";
+
+  document.getElementById("steps").appendChild(input);
+}
