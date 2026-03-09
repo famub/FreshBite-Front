a51a3fd872f83557678e380efa6664ac9928285f
Hatun05 <445201400@student.ksu.edu.sa>
2026-03-09 05:56:46 +0300

    db

--- db_connection.php
+++ db_connection.php
@@ -1,39 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";       // حسب إعداد MAMP أو XAMPP
+$password = "root";   // حسب إعداد MAMP أو XAMPP
+$dbname = "freshbit";
 
-<!DOCTYPE html>
+$conn = mysqli_connect($host, $user, $password, $dbname);
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-       
-    <?php
-    
-    // db_connection.php
-    $host = "localhost";
-    $user = "root";
-    $password = "root"; 
-    $dbname = "freshbit"; 
-
-    $conn = mysqli_connect($host, $user, $password, $dbname);
-
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
-    $sql = "SELECT * FROM user";
-$result = mysqli_query($conn, $sql);
-
-while($row = mysqli_fetch_assoc($result)) {
-    echo $row['photoFileName'] . "<br>" . $row['videoFilePath']."<br> <br>";
-       
-
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-    ?>
-        
-        
-        
-    </body>
-</html>
+mysqli_set_charset($conn, "utf8"); // ضروري للعربي
+?>
